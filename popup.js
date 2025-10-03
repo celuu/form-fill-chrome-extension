2cf62a8303bf7118df858382035fc458ffa310c6
celuu <christine.e.luu@gmail.com>
2025-10-03 15:32:53 -0700

    Update popup entry point.

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
 // the popup logic
+document.getElementById("save").addEventListener("click", () => {
+  const resume = document.getElementById("resume").value;
+  chrome.storage.local.set({ resume }, () => {
+    alert("Resume saved!");
+  });
+});
